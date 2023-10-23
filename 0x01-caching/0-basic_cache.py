45e2d5ad062570cc0e392195bc6ba932175d25f4
Torpeedofv <torpeedo.fv@gmail.com>
2023-10-23 11:32:10 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A module that contains a class that inherits from a base class"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Creation of a child class"""
+    def put(self, key, item):
+        """adds a new key and item to the cache"""
+        if key or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """gets an item using the key from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
